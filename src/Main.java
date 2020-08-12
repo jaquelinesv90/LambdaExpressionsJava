02e979b54904258e2af03acadbe31aaf5cfff932
Jaqueline <jaquelinesv90@gmail.com>
2020-08-12 19:25:52 -0300

    changes

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,37 @@ public class Main {
         // List imutável.(que não se pode alterar, sem possibilidade de mudança).
         List<User> users = Arrays.asList(user1, user2, user3);
 
-        for(User u : users){
+        for (User u : users) {
             System.out.printl(u.getName());
         }
-
-
-
     }
+        // Consumer é uma interface
+        //é uma prática comum criar classes anonimas
+        //para tarefas simples
+        class Mostrador implements Consumer<User> {
+            public void accept(User u){
+                System.out.println(u.getName());
+            }
+        };
+
+        //a classe acima pode ser simplificada
+        Consumer<User> mostrador = new Consumer<User>(){
+            public void accept(User u){
+                System.out.println(u.getName());
+            }
+        };
+
+        //utilizando lambda
+        Consumer<User> mostrador1 =
+                (User u) -> {System.out.println(u.getName());};
+
+        // pode-se ir além, o compilador consegue inferir o tipo
+        // sem a necessidade de utilizar User, nem parenteses
+        Consumer<User> mostrador2 =
+                u -> {System.out.println(u.getName());};
+
+        // escrevendo em apenas uma linha
+        Consumer<User> mostrador3 = u -> System.out.println(u.getName());
+
+
 }
